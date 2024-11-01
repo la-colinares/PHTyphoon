c9f812ba57b3e1040cede2fc6e9cb61f94a1cbf5
LA Colinares <dev.lacolinares@gmail.com>
2024-11-02 05:22:54 +0800

    added Typography

--- composeApp/src/commonMain/kotlin/com/lacolinares/phtyphoon/App.kt
+++ composeApp/src/commonMain/kotlin/com/lacolinares/phtyphoon/App.kt
@@ -5,22 +5,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.lacolinares.phtyphoon.theme.MontTypography
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import com.lacolinares.phtyphoon.Greeting
-
 import phtyphoon.composeapp.generated.resources.Res
 import phtyphoon.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    MaterialTheme(typography = MontTypography()) {
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
---
+++ composeApp/src/commonMain/kotlin/com/lacolinares/phtyphoon/theme/Font.kt
@@ -0,0 +1,41 @@
+package com.lacolinares.phtyphoon.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import org.jetbrains.compose.resources.Font
+import phtyphoon.composeapp.generated.resources.Montserrat
+import phtyphoon.composeapp.generated.resources.Res
+
+@Composable
+fun MontFontFamily() = FontFamily(
+    Font(Res.font.Montserrat, weight = FontWeight.Light),
+    Font(Res.font.Montserrat, weight = FontWeight.Normal),
+    Font(Res.font.Montserrat, weight = FontWeight.Medium),
+    Font(Res.font.Montserrat, weight = FontWeight.SemiBold),
+    Font(Res.font.Montserrat, weight = FontWeight.Bold)
+)
+
+@Composable
+fun MontTypography() = Typography().run {
+
+    val fontFamily = MontFontFamily()
+    copy(
+        displayLarge = displayLarge.copy(fontFamily = fontFamily),
+        displayMedium = displayMedium.copy(fontFamily = fontFamily),
+        displaySmall = displaySmall.copy(fontFamily = fontFamily),
+        headlineLarge = headlineLarge.copy(fontFamily = fontFamily),
+        headlineMedium = headlineMedium.copy(fontFamily = fontFamily),
+        headlineSmall = headlineSmall.copy(fontFamily = fontFamily),
+        titleLarge = titleLarge.copy(fontFamily = fontFamily),
+        titleMedium = titleMedium.copy(fontFamily = fontFamily),
+        titleSmall = titleSmall.copy(fontFamily = fontFamily),
+        bodyLarge = bodyLarge.copy(fontFamily =  fontFamily),
+        bodyMedium = bodyMedium.copy(fontFamily = fontFamily),
+        bodySmall = bodySmall.copy(fontFamily = fontFamily),
+        labelLarge = labelLarge.copy(fontFamily = fontFamily),
+        labelMedium = labelMedium.copy(fontFamily = fontFamily),
+        labelSmall = labelSmall.copy(fontFamily = fontFamily)
+    )
+}
